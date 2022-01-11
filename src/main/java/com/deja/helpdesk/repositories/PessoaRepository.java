f920c50fdb21cfcabe9c0ee0b6aa473b2e06005e
DejaLazzarotto <deja@deja.com.br>
2022-01-11 19:32:07 -0300

    exceção para CPF e EMAIL já cadastrados

--- src/main/java/com/deja/helpdesk/repositories/PessoaRepository.java
+++ src/main/java/com/deja/helpdesk/repositories/PessoaRepository.java
@@ -1,9 +1,13 @@
 package com.deja.helpdesk.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.deja.helpdesk.domain.Pessoa;
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Integer> {
 
+	Optional<Pessoa> findByCpf(String cpf);
+	Optional<Pessoa> findByEmail(String email);
 }
