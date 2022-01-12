5b916b55f11cb1d7e2328c867f4bba15db3359c4
DejaLazzarotto <deja@deja.com.br>
2022-01-12 10:45:55 -0300

    CRUD para Cliente implementado

--- src/main/java/com/deja/helpdesk/domain/Cliente.java
+++ src/main/java/com/deja/helpdesk/domain/Cliente.java
@@ -2,17 +2,19 @@ package com.deja.helpdesk.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
-import com.deja.helpdesk.domain.enums.Perfil;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.deja.helpdesk.domain.dtos.ClienteDTO;
+import com.deja.helpdesk.domain.enums.Perfil;
 
 @Entity
 public class Cliente extends Pessoa {
 	private static final long serialVersionUID = 1L;
-	
+
 	@JsonIgnore
 	@OneToMany(mappedBy = "cliente")
 	private List<Chamado> chamados = new ArrayList<>();
@@ -23,8 +25,19 @@ public class Cliente extends Pessoa {
 	}
 
 	public Cliente(Integer id, String nome, String cpf, String email, String senha) {
-		super(id, nome, cpf, email, senha);		
+		super(id, nome, cpf, email, senha);
 		addPerfil(Perfil.CLIENTE);
+	}
+
+	public Cliente(ClienteDTO obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.cpf = obj.getCpf();
+		this.email = obj.getEmail();
+		this.senha = obj.getSenha();
+		this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		this.dataCriacao = obj.getDataCriacao();
 	}
 
 	public List<Chamado> getChamados() {
