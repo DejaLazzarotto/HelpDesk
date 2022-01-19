eaa1df363cbc2553ac336ad711c3c6c775631d34
DejaLazzarotto <deja@deja.com.br>
2022-01-19 11:36:01 -0300

    adicionado mensagem CPF invalido

--- src/main/java/com/deja/helpdesk/domain/dtos/CredenciaisDTO.java
+++ src/main/java/com/deja/helpdesk/domain/dtos/CredenciaisDTO.java
@@ -18,5 +18,4 @@ public class CredenciaisDTO {
 		this.senha = senha;
 	}
 	
-	
 }
--- src/main/java/com/deja/helpdesk/domain/dtos/TecnicoDTO.java
+++ src/main/java/com/deja/helpdesk/domain/dtos/TecnicoDTO.java
@@ -8,24 +8,27 @@ import java.util.stream.Collectors;
 
 import javax.validation.constraints.NotNull;
 
+import org.hibernate.validator.constraints.br.CPF;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.deja.helpdesk.domain.Tecnico;
 import com.deja.helpdesk.domain.enums.Perfil;
-import com.fasterxml.jackson.annotation.JsonFormat;
 
 public class TecnicoDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	protected Integer id;
 	@NotNull(message = "O campo NOME é requerido")
 	protected String nome;
 	@NotNull(message = "O campo CPF é requerido")
+	@CPF
 	protected String cpf;
-	@NotNull(message = "O campo EMail é requerido")
+	@NotNull(message = "O campo EMAIL é requerido")
 	protected String email;
-	@NotNull(message = "O campo Senha é requerido")
+	@NotNull(message = "O campo SENHA é requerido")
 	protected String senha;
 	protected Set<Integer> perfis = new HashSet<>();
-	
+
 	@JsonFormat(pattern = "dd/MM/yyyy")
 	protected LocalDate dataCriacao = LocalDate.now();
 
@@ -102,5 +105,4 @@ public class TecnicoDTO implements Serializable {
 		this.dataCriacao = dataCriacao;
 	}
 
-	
 }
